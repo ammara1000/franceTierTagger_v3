56dc06ea356041ecf2e264f2f094b2aaea3045a4
Axxio <lebmichel06@gmail.com>
2026-08-14 20:39:26 +0200

    Update Francetiers_tagger.java

--- src/main/java/nerd/amara/Francetiers_tagger.java
+++ src/main/java/nerd/amara/Francetiers_tagger.java
@@ -2,7 +2,6 @@ package nerd.amara;
 
 import nerd.amara.tiers.PlayerInfo;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents;
@@ -10,37 +9,27 @@ import net.minecraft.entity.player.PlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Objects;
-
 public class Francetiers_tagger implements ClientModInitializer {
 	public static final String MOD_ID = "francetiers_tagger";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static String web_url="https://francetiers.fr/search_playerV2.php?pseudo=";
+	public static String web_url="https://francetiers.fr/search_playerV2.php?pseudo=";
 
 	@Override
 	public void onInitializeClient() {
-		Keybinds.RedgisterKeybinds();
+		Keybinds.registerKeybinds();
 		ConfigManager.load();
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
 			CommandManager.registerCommands(dispatcher);
 		});
-		String lol = "\u0000";
-		//PlayerInfo info_ = Http.getJson("https://tierlistmc.fr/search_player.php?pseudo="+"amara1000", PlayerInfo.class);
-		//if (info_ != null) {
-		//	System.out.println(ShowedTier.showed_tier(info_));
-		//}
-		ClientEntityEvents.ENTITY_LOAD.register(((entity, clientWorld) ->{
-			if (entity instanceof PlayerEntity){
-				if (((TierModifier) entity).getSuffix()==null){
-					new Thread(() -> {
-						//System.out.println(entity.getName().getString());
-						PlayerInfo info = Http.getJson(web_url+entity.getName().getString(), PlayerInfo.class);
-						if (info != null) {
-							((TierModifier)entity).setSuffix(ShowedTier.showed_tier(info));
-						}
-					}).start();
+
+		ClientEntityEvents.ENTITY_LOAD.register(((entity, clientWorld) -> {
+			if (entity instanceof PlayerEntity) {
+				if (((TierModifier) entity).getSuffix() == null) {
+					String pseudo = entity.getName().getString();
+					RequestManager.fetchPlayerInfo(pseudo,
+							(PlayerInfo info) -> ((TierModifier) entity).setSuffix(ShowedTier.showed_tier(info)),
+							(Http.Status status) -> LOGGER.warn("Impossible de récupérer le tier de {} ({})", pseudo, status));
 				}
-				//((TierModifier)entity).setSuffix(" (lol)");
 			}
 		}));
 
