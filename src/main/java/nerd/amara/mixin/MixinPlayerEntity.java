35d98e0be5bab639fd1a3599c8afa1ac99251dfd
Axxio <lebmichel06@gmail.com>
2026-08-15 17:56:23 +0200

    Optimisations

--- src/main/java/nerd/amara/mixin/MixinPlayerEntity.java
+++ src/main/java/nerd/amara/mixin/MixinPlayerEntity.java
@@ -10,28 +10,32 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(PlayerEntity.class)
 public abstract class MixinPlayerEntity implements TierModifier {
     @Shadow public abstract void remove(Entity.RemovalReason reason);
 
+    @Unique
+    private static final StyleSpriteSource.Font FRTL_FONT = new StyleSpriteSource.Font(Identifier.of("frtl", "lol"));
+
     private String suffix = null;
 
     @Override
-    public String getSuffix(){
+    public String getSuffix() {
         return suffix;
     }
 
     @Override
-    public void setSuffix(String value){
-        this.suffix=value;
+    public void setSuffix(String value) {
+        this.suffix = value;
     }
 
     @ModifyReturnValue(method = "getDisplayName", at = @At("RETURN"))
     public Text lol(Text original) {
         if (suffix != null) {
-            return original.copy().append(Text.literal(suffix).styled(s -> s.withColor(Formatting.WHITE)).styled(s -> s.withFont(new StyleSpriteSource.Font(Identifier.of("frtl","lol")))));
+            return original.copy().append(Text.literal(suffix).styled(s -> s.withColor(Formatting.WHITE).withFont(FRTL_FONT)));
         }
         return original;
     }
