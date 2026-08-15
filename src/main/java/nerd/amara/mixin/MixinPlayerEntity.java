ed9c0f6bcfa752ef8123479869014b797e8ad3b4
Axxio <lebmichel06@gmail.com>
2026-08-15 06:42:54 +0200

    Update MixinPlayerEntity.java
    Code mort remove

--- src/main/java/nerd/amara/mixin/MixinPlayerEntity.java
+++ src/main/java/nerd/amara/mixin/MixinPlayerEntity.java
@@ -2,8 +2,6 @@ package nerd.amara.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import nerd.amara.TierModifier;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.Font;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.StyleSpriteSource;
@@ -19,21 +17,21 @@ public abstract class MixinPlayerEntity implements TierModifier {
     @Shadow public abstract void remove(Entity.RemovalReason reason);
 
     private String suffix = null;
+
     @Override
     public String getSuffix(){
         return suffix;
     }
+
     @Override
     public void setSuffix(String value){
         this.suffix=value;
     }
+
     @ModifyReturnValue(method = "getDisplayName", at = @At("RETURN"))
     public Text lol(Text original) {
-        PlayerEntity self = (PlayerEntity) (Object) this;
-        //String username = self.getName().getString();
-        String sufix = this.suffix;
-        if (suffix!=null) {
-            return original.copy().append(Text.literal(sufix).styled(s -> s.withColor(Formatting.WHITE)).styled(s -> s.withFont(new StyleSpriteSource.Font(Identifier.of("frtl","lol")))));
+        if (suffix != null) {
+            return original.copy().append(Text.literal(suffix).styled(s -> s.withColor(Formatting.WHITE)).styled(s -> s.withFont(new StyleSpriteSource.Font(Identifier.of("frtl","lol")))));
         }
         return original;
     }
